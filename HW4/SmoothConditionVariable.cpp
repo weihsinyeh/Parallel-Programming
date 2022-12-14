09668ee3a82a297ac8c543df40c22cb32b33e154
cindy917 <90430653+weihsinyeh@users.noreply.github.com>
2022-12-15 07:29:01 +0800

    Update SmoothConditionVariable.cpp

--- HW4/SmoothConditionVariable.cpp
+++ HW4/SmoothConditionVariable.cpp
@@ -57,19 +57,19 @@ void * child(void *arg){
             pthread_mutex_unlock(&barrier_mutex);
                 
             if(count == 0){cout<<threadID<<endl;}
-		for(int i = start; i<end; i++){
-			for(int j =0; j<width ; j++){
+			for(int i = start; i<end; i++){
+				for(int j =0; j<width ; j++){
 
-				int Top   = i>0 ? i-1 : height-1;
-				int Down  = i<height-1 ? i+1 : 0;
-				int Left  = j>0 ? j-1 : width-1;
-				int Right = j<width-1 ? j+1 : 0;
+					int Top   = i>0 ? i-1 : height-1;
+					int Down  = i<height-1 ? i+1 : 0;
+					int Left  = j>0 ? j-1 : width-1;
+					int Right = j<width-1 ? j+1 : 0;
 	
     				BMPSaveData[i][j].rgbBlue =  (double) (BMPData[i][j].rgbBlue+BMPData[Top][j].rgbBlue+BMPData[Top][Left].rgbBlue+BMPData[Top][Right].rgbBlue+BMPData[Down][j].rgbBlue+BMPData[Down][Left].rgbBlue+BMPData[Down][Right].rgbBlue+BMPData[i][Left].rgbBlue+BMPData[i][Right].rgbBlue)/9+0.5;
 	    			BMPSaveData[i][j].rgbGreen =  (double) (BMPData[i][j].rgbGreen+BMPData[Top][j].rgbGreen+BMPData[Top][Left].rgbGreen+BMPData[Top][Right].rgbGreen+BMPData[Down][j].rgbGreen+BMPData[Down][Left].rgbGreen+BMPData[Down][Right].rgbGreen+BMPData[i][Left].rgbGreen+BMPData[i][Right].rgbGreen)/9+0.5;
 		    		BMPSaveData[i][j].rgbRed =  (double) (BMPData[i][j].rgbRed+BMPData[Top][j].rgbRed+BMPData[Top][Left].rgbRed+BMPData[Top][Right].rgbRed+BMPData[Down][j].rgbRed+BMPData[Down][Left].rgbRed+BMPData[Down][Right].rgbRed+BMPData[i][Left].rgbRed+BMPData[i][Right].rgbRed)/9+0.5;
-			}
-            	}
+				}
+            }
             //way2
             /*
             for(int i = start; i<end; i++){

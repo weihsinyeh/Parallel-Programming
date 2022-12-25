f73a36dbcb3c0f8cfd347a0e25fac344e722de62
cindy917 <90430653+weihsinyeh@users.noreply.github.com>
2022-12-25 10:44:08 +0800

    Update h5_problem1.cpp

--- HW5/h5_problem1.cpp
+++ HW5/h5_problem1.cpp
@@ -2,34 +2,41 @@
 #include<omp.h>
 #include<fstream>
 #include<ctime>
-#include<stdlib.h>
+#include<fstream>
+#include <cstdlib>
 using namespace std;
 #pragma warning(disable : 4996)
 int cmp(const void * a, const void * b){
     return *(int *)a - *(int *)b;
 }
 int main(int argc,char **argv){
-    if(argc<2)return 0;
+    srand(time(NULL));
+    
     int i,j,count,n;
     cin >> n;
     int *a = new int[n];
     int *b = new int[n];
     int *c = new int[n];
     int *temp = new int[n];
-    for(i = 0;i < n;i++) cin >> b[i];
+    for(i = 0;i < n;i++)  b[i] = rand()%(n);
     for(i = 0;i < n;i++){
         a[i] = b[i];
         c[i] = b[i];
     }
+    for(i = 0;i < n;i++) cout << b[i] <<" ";
     clock_t starttime,endtime;
+
     /*** serial-qsort ***/
+
     starttime = clock();
     qsort(c,n,sizeof(int),cmp);
     endtime = clock();
     cout << "Total time serial-qsort: " << (double)(endtime - starttime)/CLOCKS_PER_SEC<<endl;
+
     /*** serial-qsort ***/
     
     /*** serial ***/
+
     starttime = clock();
     for(i = 0;i < n;i++){
         count = 0;
@@ -42,9 +49,11 @@ int main(int argc,char **argv){
     for(i = 0;i < n;i++)a[i] = temp[i];
     endtime = clock();
     cout << "Total time serial: " << (double)(endtime - starttime)/CLOCKS_PER_SEC<<endl;
+
     /*** serial ***/
 
     /*** parallel ***/
+    
     starttime = clock();
 #pragma omp parallel for private(i,j,count) shared(n,a,temp)
     for(i = 0;i < n;i++){

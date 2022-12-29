c5921396e4351b613feb276d985a8e72b62cef5d
cindy917 <90430653+weihsinyeh@users.noreply.github.com>
2022-12-29 16:14:59 +0800

    Update input.c

--- HW5/input.c
+++ HW5/input.c
@@ -23,7 +23,7 @@ int main(int argc,char * argv[]) {
    int num[3] = {3334,3333,3333};
 
 
-   FILE *writeMatrix = fopen("test_result.txt","w");
+   FILE *writeMatrix = fopen("test100.txt","w");
    fprintf(writeMatrix,"%d %d\n",100,100);
    for(int i=0;i<100;i++){
         for(int j=0;j<100;j++){
@@ -40,30 +40,6 @@ int main(int argc,char * argv[]) {
       fprintf(writeMatrix,"\n");
    }
    fclose(writeMatrix);
-   /*
-   char buf[100];
-	int fd = open("/proc/proc_test", O_RDWR);
-
-	read(fd, buf, 100);
-	lseek(fd, 0 , SEEK_SET);
-	write(fd, "33 4", 5);
-	
-	lseek(fd, 0 , SEEK_SET);
-	read(fd, buf, 100);
-   */
-
    
-   // 取得計算結果
-   /*
-   FILE *writeMatrix = fopen("test_result404053.txt","w");
-   fprintf(writeMatrix,"%d %d\n",row1,col2);
-   for(int i=0;i<row1;i++){
-      for(int j=0;j<col2;j++)
-         fprintf(writeMatrix,"%d ",result[i*col2 + j]);
-      fprintf(writeMatrix,"\n");
-   }
-   fclose(writeMatrix);
-   free(result); // 釋放記憶體
-   */
    return 0;
 }
